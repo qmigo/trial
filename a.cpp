a65e6515308e42a318e99e79b9c594269b1b58f2
qmigo <ankur031100@gmail.com>
2022-05-03 01:46:40 +0530

    first file

---
+++ a.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+#define int long long
+using namespace std;
+int M = 1e9;
+int bash[100000];
+void solve()
+{
+    
+}
+int32_t main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+        {
+            solve();
+        }
+}
